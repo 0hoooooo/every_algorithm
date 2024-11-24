ed3544b9b520a256724744d2948fd1115a7da35f
0hoooooo <youngho7482@naver.com>
2024-11-24 23:36:27 +0900

    20241124_공찾기

---
+++ 20241124/InsertBall.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class InsertBall {
+    public static void main(String[] args) throws IOException{
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] basket = new int[N];
+        int count = 1;
+
+        while(count <= M) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            while(i <= j) {
+                basket[i - 1] = k;
+                i++;
+            }
+            count++;
+        }
+        for (int i = 0; i < basket.length; i++) {
+            System.out.printf("%d", basket[i]);
+            if (i != basket.length - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
